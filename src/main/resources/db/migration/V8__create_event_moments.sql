ec6ad8bb58cabce1f1049b17b2282d7531f49c4a
Jess Watson <watsonjess@proton.me>
2026-03-17 16:13:38 +0000

    changed moment_id and gitignore

--- src/main/resources/db/migration/V8__create_event_moments.sql
+++ src/main/resources/db/migration/V8__create_event_moments.sql
@@ -3,5 +3,5 @@ CREATE TABLE event_moments (
     event_id    BIGINT    NOT NULL REFERENCES events(id) ON DELETE CASCADE,
     moment_id     BIGINT    NOT NULL REFERENCES moments(id) ON DELETE CASCADE,
     added_at    TIMESTAMP NOT NULL DEFAULT NOW(),
-    UNIQUE (event_id, item_id)
+    UNIQUE (event_id, moment_id)
 );
