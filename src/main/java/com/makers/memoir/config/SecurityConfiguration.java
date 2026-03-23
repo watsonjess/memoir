b510cff353b690972ca50d03de44a42157f78528
mark-addis <54816211+mark-addis@users.noreply.github.com>
2026-03-23 14:25:43 +0000

    Start logout stuff

--- src/main/java/com/makers/memoir/config/SecurityConfiguration.java
+++ src/main/java/com/makers/memoir/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.makers.memoir.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,8 +14,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Value("${spring.security.oauth2.client.provider.auth0.issuer-uri}")
+    private String issuer;
+
+    @Value("${spring.security.oauth2.client.registration.auth0.client-id}")
+    private String clientId;
+
+    @Value("${app.base-url:http://localhost:8080}")
+    private String returnTo;
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+
 
         HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
         requestCache.setRequestMatcher(request -> {
@@ -31,6 +43,11 @@ public class SecurityConfiguration {
                 .oauth2Login(oauth -> oauth
                         .successHandler((request, response, authentication) -> {
                             response.sendRedirect("/after-login");
+                        })
+                ).logout(logout -> logout
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.sendRedirect(issuer + "v2/logout?returnTo="
+                                    + returnTo + "&client_id=" + clientId);
                         })
                 );
 
